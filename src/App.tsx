6b23088cbbb71793d0eecb462d6063d23b883fa7
alinecarvalhopro <alinecarvalho.pro@gmail.com>
2023-05-13 19:20:43 -0300

    feat: toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MyRoutes } from "./routes";
 import { UserProvider } from "./providers/UserContext";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
+      <Toaster position="bottom-right" reverseOrder={false} />
       <GlobalStyle />
       <UserProvider>
         <MyRoutes />
